7453df92651560cc294ab1dfaed499cbba81298a
jjatencia <jjatencia@gmail.com>
2025-08-25 20:56:49 +0200

    Usa cadena al redirigir en la página raíz

--- app/page.tsx
+++ app/page.tsx
@@ -7,5 +7,5 @@ export default async function Home() {
   const url = headers().get("x-url") ?? process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
   const base = url.endsWith("/") ? url : `${url}/`;
   const destination = new URL(session ? "agenda" : "login", base);
-  redirect(destination);
+  redirect(destination.toString());
 }
