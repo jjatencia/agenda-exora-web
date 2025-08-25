0627ebd3779ef47fb42394bbc9af53214645a305
jjatencia <jjatencia@gmail.com>
2025-08-25 21:22:38 +0200

    refactor: use relative redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 
 export default async function Home() {
   const session = await auth();
-  const url = headers().get("x-url") ?? process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
-  const base = url.endsWith("/") ? url : `${url}/`;
-  const destination = new URL(session ? "agenda" : "login", base);
-  redirect(destination.toString());
+  redirect(session ? "/agenda" : "/login");
 }
